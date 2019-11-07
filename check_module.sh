3862aae97e981d181449545bff51cd2f8db8b8c4
Jack Over <jack@limoengroen.nl>
2019-11-07 15:56:00 +0100

    Added option flags etc see -h flag

--- check_module.sh
+++ check_module.sh
@@ -1,7 +1,53 @@
 #!/bin/bash
 
-Echo 'What module would you like to check? (input the system name of the module)'
-read modulename
+function usage() {
+cat << EOF
+usage: $0 options
+
+OPTIONS:
+  -h    Show this message (optional)
+  -m    Give the module machine name as parameter input (optional)
+  -c    Copy results to clipboard
+  -o    Open an issue on drupal.org if errors found
+  -u    Update existing module
+
+EOF
+}
+
+while getopts "hm:cou" OPTION
+  do
+    case "$OPTION" in
+      h)
+        usage
+        exit 1;
+        ;;
+      m)
+        modulename="$OPTARG"
+        ;;
+      c)
+        copy_flag=1
+        ;;
+      o)
+        open_flag=1
+        ;;
+      u)
+        update_flag=1
+        ;;
+      ?)
+        usage
+        exit 1;
+        ;;
+    esac
+done
+
+# we need the script to be executed from current dir.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# if the module name is not set, ask for a module name.
+if [ -z ${modulename+x} ]; then
+  Echo 'What module would you like to check? (input the system name of the module)'
+  read modulename
+fi
 
 DC_INSTALLED="$(drupal-check --version)"
 
@@ -14,18 +60,36 @@ DC_INSTALLED="$(drupal-check --version)"
 
 Echo $DC_INSTALLED installed
 
-# we need the script to be executed from current dir.
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+# Install drupal.
 cd ~/
-
 composer create-project drupal-composer/drupal-project:8.x-dev clean-drupal --no-interaction --stability=dev
-
 cd clean-drupal
-
 composer config prefer-stable false
-composer require drupal/$modulename
+
+if ls ~/clean-drupal/web/modules/contrib | grep -q "$modulename"; then
+  if [[ "$update_flag" -eq "1" ]]; then
+    # Update module
+    echo "reinstalling module"
+    composer require drupal/$modulename -o
+  fi
+else
+  echo "installing module"
+  # Instal module
+  composer require drupal/$modulename -o
+fi
 
 cd $DIR
 
-drupal-check ~/clean-drupal/web/modules/contrib/$modulename/
+drupal-check ~/clean-drupal/web/modules/contrib/$modulename/ | tee ~/drupal-checked/$modulename.txt
+
+Echo results should also be available in ~/drupal-checked/$modulename.txt
+
+if [[ "$copy_flag" -eq "1" ]]; then
+  cat ~/drupal-checked/$modulename.txt | pbcopy
+fi
+
+if [[ "$open_flag" -eq "1" ]]; then
+  if cat ~/drupal-checked/$modulename.txt | grep -q '\[ERROR\]'; then
+     open https://www.drupal.org/node/add/project-issue/$modulename?title=Drupal%209%20Deprecated%20Code%20Report&tags=Drupal%209%20compatibility
+  fi
+fi
